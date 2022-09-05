0218a370b334c2ff78c2c1f3fe83aed3e23dfbe6
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-09-04 21:23:56 -0700

    redid the index.ts
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
-import {
-  commands,
-  CompleteResult,
-  ExtensionContext,
-  LanguageClient,
-  listManager,
-  sources,
-  window,
-  services,
-  workspace,
-} from 'coc.nvim';
-import DemoList from './lists';
+import { ExtensionContext, services, workspace, LanguageClient } from 'coc.nvim';
 
 export async function activate(context: ExtensionContext): Promise<void> {
-  const serverOtpions = {
+  const serverOptions = {
     command: 'grain lsp',
   };
 
@@ -20,57 +9,7 @@ export async function activate(context: ExtensionContext): Promise<void> {
     documentSelector: ['grain'],
   };
 
-  const client = new LanguageClient('coc-grain', 'coc-grain', serverOtpions, clientOptions);
+  const client = new LanguageClient('coc-grain', 'coc-grain', serverOptions, clientOptions);
 
   context.subscriptions.push(services.registLanguageClient(client));
-
-  window.showMessage(`coc-grain works!`);
-
-  context.subscriptions.push(
-    commands.registerCommand('coc-grain.Command', async () => {
-      window.showMessage(`coc-grain Commands works!`);
-    }),
-
-    listManager.registerList(new DemoList(workspace.nvim)),
-
-    sources.createSource({
-      name: 'coc-grain completion source', // unique id
-      doComplete: async () => {
-        const items = await getCompletionItems();
-        return items;
-      },
-    }),
-
-    workspace.registerKeymap(
-      ['n'],
-      'grain-keymap',
-      async () => {
-        window.showMessage(`registerKeymap`);
-      },
-      { sync: false }
-    ),
-
-    workspace.registerAutocmd({
-      event: 'InsertLeave',
-      request: true,
-      callback: () => {
-        window.showMessage(`registerAutocmd on InsertLeave`);
-      },
-    })
-  );
-}
-
-async function getCompletionItems(): Promise<CompleteResult> {
-  return {
-    items: [
-      {
-        word: 'TestCompletionItem 1',
-        menu: '[coc-grain]',
-      },
-      {
-        word: 'TestCompletionItem 2',
-        menu: '[coc-grain]',
-      },
-    ],
-  };
 }
